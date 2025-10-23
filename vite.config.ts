f8f2d99dd317cdb61b2ba8b065bd2f65085050fe
Giorgio Garasto <giorgio@garasto.me>
2025-10-23 09:45:38 +0200

    perf: improve index html compression

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 import { VitePWA } from 'vite-plugin-pwa';
 import { sitemap } from './vite-plugins/sitemap';
 import packageJson from './package.json' with { type: 'json' };
+import type { WebSite } from 'schema-dts';
 
 process.env.VITE_APP_VERSION = packageJson.version;
 
@@ -14,7 +15,63 @@ export default defineConfig({
   plugins: [
     mkcert(),
     preload(),
-    createHtmlPlugin({ minify: true }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          linkedData: JSON.stringify({
+            '@context': 'http://schema.org/',
+            '@type': 'WebSite',
+            url: 'https://clashroyalecardmaker.com',
+            image: 'https://clashroyalecardmaker.com/icons/any-512x512.png',
+            name: 'Clash Royale Card Maker',
+            description:
+              'Welcome to Clash Royale Card Maker! With this simple tool you can create your own custom Clash Royale cards!',
+            author: [
+              {
+                '@type': 'Person',
+                url: 'https://giorgio.garasto.me',
+                image: 'https://giorgio.garasto.me/images/propic-20220630.jpg',
+                name: 'Dabolus',
+                givenName: 'Giorgio',
+                familyName: 'Garasto',
+                gender: { '@type': 'GenderType', name: 'Male' },
+                email: 'giorgio@garasto.me',
+                nationality: { '@type': 'Country', name: 'IT' },
+                description:
+                  'Giorgio Garasto—also known as Dabolus—is an italian Software Engineer and Graphic Designer specialized in web development. He is mainly known for his very first published project, the Clash Royale Card Maker.',
+                jobTitle: 'Software Engineer',
+                worksFor: {
+                  '@type': 'Organization',
+                  name: 'Empatica',
+                  address: 'Via Stendhal, 36, 20144 Milano MI, Italy',
+                },
+                sameAs: [
+                  'https://www.linkedin.com/in/giorgiogarasto',
+                  'https://github.com/Dabolus',
+                  'https://x.com/Dabolus',
+                  'https://fb.me/giorgio.garasto',
+                  'https://t.me/Dabolus',
+                ],
+              },
+            ],
+          } satisfies WebSite & { '@context': string }),
+        },
+      },
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processScripts: ['application/ld+json'],
+      },
+    }),
     sitemap(),
     VitePWA({
       // Don't inject register, as we will do it ourselves
